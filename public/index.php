7defb0e596b6f545cedbf5ce80327f33afe97137
KHACHIAI Oussama <oussama.khachiai@gmail.com>
2020-05-11 15:48:27 +0200

    Adding the phpcs to the project

--- public/index.php
+++ public/index.php
@@ -4,9 +4,7 @@ require '../vendor/autoload.php';
 
 use function Http\Response\send;
 
-$app = new \Framework\App(
-
-);
+$app = new \Framework\App();
 
 $response = $app->run(\GuzzleHttp\Psr7\ServerRequest::fromGlobals());
 
--- src/Framework/App.php
+++ src/Framework/App.php
@@ -12,14 +12,13 @@ class App
 {
     public function run(RequestInterface $request): ResponseInterface
     {
-        $uri = $request->getUri()->getPath();
+        $uri      = $request->getUri()->getPath();
         $response = new Response();
         if ($uri !== null && $uri[-1] === "/") {
             return $response->withStatus(301)
                 ->withHeader('Location', substr($uri, 0, -1));
         }
-        if ($uri === "/blog")
-        {
+        if ($uri === "/blog") {
             return new Response(200, [], "<h1>Welcome on the blog page</h1>");
         }
 
